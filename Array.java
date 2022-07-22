b93f94b5e5575a04659dfc961822827f195512db
Jagadheeshwaran98 <108801791+Jagadheeshwaran98@users.noreply.github.com>
2022-07-22 09:24:46 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,29 @@
+//@author : Jagadheeshwaran 
+
+public class Array 
+{     
+public static void main(String[] args)
+      {
+             string[] names = {siva, karthik, mano};              
+
+                 string element = mano;        
+           
+                 string index = -1;                   
+          
+             for(string i = 0; i < names.length; i++) 
+
+         {             
+             if(names[i] == element)                         
+                    {
+                         index = i;                 
+             
+                          break;          
+          
+                               }       
+
+          }                   
+             
+System.out.println("Index of "+element+" is : "+index);   
+  
+       }
+ } 
