ad366e64f1233d96718304b6530d12cfb06cb0c0
Jagadheeshwaran98 <108801791+Jagadheeshwaran98@users.noreply.github.com>
2022-07-29 12:18:00 +0530

    Add files via upload

---
+++ GetEployeesBank.java
@@ -0,0 +1,39 @@
+/*
+    
+   @author  : Jagadheeshwaran
+
+    Title   : constructor
+    
+    Question: customer bank details
+
+*/
+
+class Bank {
+	private String name;
+	Bank(String name) {
+		this.name = name;
+	}
+	public String getBankName() {
+		return this.name;
+	}
+}
+
+class Employee {
+	private String name;
+	Employee(String name) {
+		this.name = name;
+	}
+	public String getEmployee() {
+		return this.name;
+	}
+}
+
+public class GetEployeesBank{
+
+	public static void main(String[] args) 
+	{
+		Bank bank = new Bank("Axis Bank");
+		Employee emp = new Employee("Neha");
+		System.out.println(emp.getEmployee() + " is employee of " + bank.getBankName());
+	}
+}
